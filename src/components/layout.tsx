3133144c06bce3effd498fe30c5cd4c8692c668a
Marcos Antonio Bezerra Carvalho Jr <houdiniam@gmail.com>
2024-03-07 14:59:11 -0300

    Conserta bug

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -4,7 +4,13 @@ import React from "react";
 import { ThemeProvider } from "@material-tailwind/react";
 
 export function Layout({ children }: { children: React.ReactNode }) {
-  return <ThemeProvider>{children}</ThemeProvider>;
+  return (
+    <ThemeProvider>
+      <div>
+      {children}
+      </div>
+    </ThemeProvider>
+    )
 }
 
 export default Layout;
